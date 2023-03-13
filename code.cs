c1e8e7c6118c65f72445d1030a18b3a377c835f7
Harsat <121182309+HarsatMKad@users.noreply.github.com>
2023-03-13 14:34:30 +0700

    name fix

--- code.cs
+++ code.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
 {
   class Memento
   {
-    public string text { get; set; }
+    public string Text { get; set; }
   }
   public interface IOriginator
   {
@@ -20,100 +20,99 @@ namespace ConsoleApp1
   }
 
   [Serializable]
-  public class txtFile : IOriginator
+  public class TxtFile : IOriginator
   {
-    public string text;
-    public string tags;
+    public string Text;
+    public string Tags;
 
-    public txtFile() { }
+    public TxtFile() { }
 
-    public txtFile(string text, string tags)
+    public TxtFile(string Text, string Tags)
     {
-      this.text = text;
-      this.tags = tags;
+      this.Text = Text;
+      this.Tags = Tags;
     }
 
     public string BinarySerialize()
     {
-      string FileName = "file.dat";
-      FileStream fs = new FileStream(FileName, FileMode.OpenOrCreate, FileAccess.Write);
-      BinaryFormatter bf = new BinaryFormatter();
-      bf.Serialize(fs, this);
-      fs.Flush();
-      fs.Close();
+      string FileName = "File.dat";
+      FileStream fileStream = new FileStream(FileName, FileMode.OpenOrCreate, FileAccess.Write);
+      BinaryFormatter binaryFormat = new BinaryFormatter();
+      binaryFormat.Serialize(fileStream, this);
+      fileStream.Flush();
+      fileStream.Close();
       return FileName;
     }
 
     public void BinaryDeserialize(string FileName)
     {
-      FileStream fs = new FileStream(FileName, FileMode.OpenOrCreate, FileAccess.Read);
-      BinaryFormatter bf = new BinaryFormatter();
-      txtFile deserialized = (txtFile)bf.Deserialize(fs);
-      text = deserialized.text;
-      fs.Close();
+      FileStream fileStream = new FileStream(FileName, FileMode.OpenOrCreate, FileAccess.Read);
+      BinaryFormatter binaryFormat = new BinaryFormatter();
+      TxtFile deserialized = (TxtFile)binaryFormat.Deserialize(fileStream);
+      Text = deserialized.Text;
+      fileStream.Close();
     }
 
-    static public string XMLSerialize(txtFile details)
+    static public string XMLSerialize(TxtFile Details)
     {
-      string FileName = "file.xml";
-      XmlSerializer serializer = new XmlSerializer(typeof(txtFile));
-      using (TextWriter writer = new StreamWriter(FileName))
+      string FileName = "File.xml";
+      XmlSerializer serializer = new XmlSerializer(typeof(TxtFile));
+      using (TextWriter Writer = new StreamWriter(FileName))
       {
-        serializer.Serialize(writer, details);
+        serializer.Serialize(Writer, Details);
       }
       return FileName;
     }
 
-    static public txtFile XMLDeserialize(string FileName)
+    static public TxtFile XMLDeserialize(string FileName)
     {
-      XmlSerializer deserializer = new XmlSerializer(typeof(txtFile));
+      XmlSerializer deserializer = new XmlSerializer(typeof(TxtFile));
       TextReader reader = new StreamReader(FileName);
       object obj = deserializer.Deserialize(reader);
-      txtFile XmlData = (txtFile)obj;
+      TxtFile XmlData = (TxtFile)obj;
       reader.Close();
       return XmlData;
     }
 
     public void PrintText()
     {
-      Console.WriteLine(text);
+      Console.WriteLine(Text);
     }
 
     object IOriginator.GetMemento()
     {
-      return new Memento { text = this.text };
+      return new Memento { Text = this.Text };
     }
     void IOriginator.SetMemento(object memento)
     {
       if (memento is Memento)
       {
-        var mem = memento as Memento;
-        text = mem.text;
+        Text = (memento as Memento).Text;
       }
     }
   }
   public class Caretaker
   {
     private object memento;
-    public void SaveState(IOriginator originator)
+    public void SaveState(IOriginator Originator)
     {
-      memento = originator.GetMemento();
+      memento = Originator.GetMemento();
     }
 
-    public void RestoreState(IOriginator originator)
+    public void RestoreState(IOriginator Originator)
     {
-      originator.SetMemento(memento);
+      Originator.SetMemento(memento);
     }
   }
 
   class FileSearch
   {
     public string FoundFiles = "";
-    public void Search(txtFile[] library, string Request, int numberOfFiles)
+    public void Search(TxtFile[] Library, string Request, int NumberOfFiles)
     {
-      for (int FileNumber = 0; FileNumber < numberOfFiles; ++FileNumber)
+      for (int FileNumber = 0; FileNumber < NumberOfFiles; ++FileNumber)
       {
-        if (library[FileNumber].tags == Request)
+        if (Library[FileNumber].Tags == Request)
         {
           FoundFiles += FileNumber + " ";
         }
@@ -136,55 +135,53 @@ namespace ConsoleApp1
     {
 
       const int NumberOfFiles = 5;
-      txtFile[] Library = new txtFile[NumberOfFiles];
-      txtFile file;
+      TxtFile[] Library = new TxtFile[NumberOfFiles];
+      TxtFile File;
 
-      file = new txtFile("какой то текст первого файла", "тег3");
-      Library[0] = file;
-      file = new txtFile("какой то текст второго файла", "тег8");
-      Library[1] = file;
-      file = new txtFile("какой то текст третьего файла", "тег3");
-      Library[2] = file;
-      file = new txtFile("какой то текст четвертого файла", "тег5");
-      Library[3] = file;
-      file = new txtFile("какой то текст пятого файла", "тег1");
-      Library[4] = file;
+      File = new TxtFile("какой то текст первого файла", "тег3");
+      Library[0] = File;
+      File = new TxtFile("какой то текст второго файла", "тег8");
+      Library[1] = File;
+      File = new TxtFile("какой то текст третьего файла", "тег3");
+      Library[2] = File;
+      File = new TxtFile("какой то текст четвертого файла", "тег5");
+      Library[3] = File;
+      File = new TxtFile("какой то текст пятого файла", "тег1");
+      Library[4] = File;
 
-      Console.WriteLine("файл с каким тегом необходим: ");
+      Console.WriteLine("Файл с каким тегом необходим: ");
       string Request = Convert.ToString(Console.ReadLine());
+        
+      FileSearch FileSearch = new FileSearch();
+      FileSearch.Search(Library, Request, NumberOfFiles);
+      Console.WriteLine(FileSearch.FoundFiles);
 
-      FileSearch filesearch = new FileSearch();
-      filesearch.Search(Library, Request, NumberOfFiles);
-      Console.WriteLine(filesearch.FoundFiles);
-
-      Console.WriteLine("выберите файл, который необходимо отредактировать: ");
+      Console.WriteLine("Выберите файл, который необходимо отредактировать: ");
       int FileNumber = Convert.ToInt32(Console.ReadLine());
 
-      string FileName = txtFile.XMLSerialize(Library[FileNumber]);
+      string FileName = TxtFile.XMLSerialize(Library[FileNumber]);
 
       Console.WriteLine("текст этого файла:");
-      Caretaker ct = new Caretaker();
+      Caretaker Caretaker = new Caretaker();
       Library[FileNumber].PrintText();
-      ct.SaveState(Library[FileNumber]);
+      Caretaker.SaveState(Library[FileNumber]);
 
       Console.WriteLine("Введите новый текст файла: ");
       string NewText = Convert.ToString(Console.ReadLine());
-      Library[FileNumber].text = NewText;
-      FileName = txtFile.XMLSerialize(Library[FileNumber]);
+      Library[FileNumber].Text = NewText;
+      FileName = TxtFile.XMLSerialize(Library[FileNumber]);
 
-      Console.WriteLine("сохранить ?(да/нет)");
+      Console.WriteLine("Файл сохранен и стал:");
+      Library[FileNumber].PrintText();
+
+      Console.WriteLine("Отменить изменения ?(да/нет)");
       string SaveChoice = Convert.ToString(Console.ReadLine());
 
-      if (SaveChoice == "нет")
+      if (SaveChoice == "да")
       {
-        ct.RestoreState(Library[FileNumber]);
-        FileName = txtFile.XMLSerialize(Library[FileNumber]);
-        Console.WriteLine("файл остался прежним: ");
-        Library[FileNumber].PrintText();
-      }
-      else
-      {
-        Console.WriteLine("файл сохранен и стал:");
+        Caretaker.RestoreState(Library[FileNumber]);
+        FileName = TxtFile.XMLSerialize(Library[FileNumber]);
+        Console.WriteLine("Файл остался прежним: ");
         Library[FileNumber].PrintText();
       }
 
